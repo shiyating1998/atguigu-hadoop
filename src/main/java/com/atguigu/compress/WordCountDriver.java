a87b1d25e52abc4999950d52b79666f17e682049
Jennifer Shi <shiyating1998@gmail.com>
2024-03-08 18:12:37 -0500

    compress algor

---
+++ src/main/java/com/atguigu/compress/WordCountDriver.java
@@ -0,0 +1,57 @@
+package com.atguigu.compress;
+
+
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.BZip2Codec;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCountDriver {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        Configuration conf = new Configuration();
+
+        // 开启map端输出压缩
+        conf.setBoolean("mapreduce.map.output.compress", true);
+
+        // 设置map端输出压缩方式
+        conf.setClass("mapreduce.map.output.compress.codec", BZip2Codec.class,CompressionCodec.class);
+
+        Job job = Job.getInstance(conf);
+
+        job.setJarByClass(WordCountDriver.class);
+
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.setInputPaths(job, new Path("C:\\Resources\\Hadoop\\资料\\11_input\\input_test.txt"));
+        FileOutputFormat.setOutputPath(job, new Path("C:\\Resources\\Hadoop\\资料\\11_input\\output888"));
+
+
+        // 设置reduce端输出压缩开启
+        FileOutputFormat.setCompressOutput(job, true);
+
+        // 设置压缩的方式
+        FileOutputFormat.setOutputCompressorClass(job, BZip2Codec.class);
+//	    FileOutputFormat.setOutputCompressorClass(job, GzipCodec.class);
+//	    FileOutputFormat.setOutputCompressorClass(job, DefaultCodec.class);
+
+        boolean result = job.waitForCompletion(true);
+
+        System.exit(result ? 0 : 1);
+    }
+}
